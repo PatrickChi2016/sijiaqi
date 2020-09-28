169fa80620962e29802290481107d519753b6ee1
patrick <patrick@192.168.31.165>
2020-09-28 15:08:47 +0800

    commit

---
+++ eureka-client/src/main/java/com/example/server/EurekaClientApplication.java
@@ -0,0 +1,21 @@
+package com.example.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+	
+	@RequestMapping
+	public String sayHello() {
+		return "hello world";
+	}
+
+}
